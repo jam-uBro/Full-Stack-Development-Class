2cde8dd6227075b22bdd6c1dbbdf8d0dc6d0ed3b
jam-uBro <mdjamilhossain786@yahoo.com>
2025-04-21 14:25:39 +0530

    Add files via upload

---
+++ Signifiya Registration Website/scr/model/register.js
@@ -0,0 +1,25 @@
+
+const mongoose = require("mongoose");
+
+const userData = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    event: {
+        type: String,
+        required: true
+    }
+});
+
+const Register = mongoose.model("userData", userData);
+
+module.exports = Register;
